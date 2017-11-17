ad2195d915ccef3e7594dab580d7752cc19136dc
Bora Ozarslan <borako.ozarslan@gmail.com>
2017-11-16 21:23:10 -0500

    Add favicon and deploy script

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const favicon = require('serve-favicon')
 const app = express()
+const path = require('path')
 
-app.get('/', (req, res) => {
-  res.send("Wohoo!")
-})
+const staticAssets = __dirname + "/public"
+const faviconPath = path.join(__dirname, 'public', 'favicon.ico')
+
+app
+  .use(express.static(staticAssets))
+  .use(favicon(faviconPath))
+  .get('/', (req, res) => {
+    res.send("Testo!")
+  })
 
 app.listen(21517, () => console.log('[INFO] Running server on port 21517'))
 
